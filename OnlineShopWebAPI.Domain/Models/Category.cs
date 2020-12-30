fe1559184e144e0f937d9b9231366e2b6465f2a1
Kristina <headwayup9@gmail.com>
2020-12-30 14:48:55 +0200

    separate Domain and Core layers

--- OnlineShopWebAPI.Domain/Models/Category.cs
+++ OnlineShopWebAPI.Domain/Models/Category.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace OnlineShopWebAPI.Domain.Models
+namespace OnlineShopWebAPI.Core.Models
 {
     class Category
     {
